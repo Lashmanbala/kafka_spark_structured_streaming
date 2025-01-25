ac82ec41b8da9d36e286c05c66a060c883a03ccb
lashmanbala <lashmanbala@gmail.com>
2025-01-25 09:03:32 +0000

    Implemented logging

--- app.py
+++ app.py
@@ -2,35 +2,44 @@ import time
 import random
 import json
 from quixstreams import Application
+import logging
 
-
-transaction_counter = 1
 
 customer_id = [f"cust_{i}" for i in range(1, 6)]      
 merchant_id = [f"merch_{i}" for i in range(1, 4)]
 product_id = [f"prod_{i}" for i in range(1, 8)] 
 
-app = Application(
-    broker_address='localhost:9092',
-    loglevel='DEBUG',
-)
+def main():
+    transaction_counter = 1
 
-try:
-    while True:
-        event = {'transaction_id':f'trans_{transaction_counter}',
-            'customer_id' : random.choice(customer_id),
-            'timestamp' : time.time(),
-            'product_id' : random.choice(product_id),
-            'amount' : random.randint(100, 1000),
-            'merchant_id' : random.choice(merchant_id)
-            }
-    
-        with app.get_producer() as producer:
-            producer.produce(topic='test-topic', value=json.dumps(event))
+    app = Application(
+        broker_address='localhost:9092',
+        loglevel='DEBUG',
+    )
 
-        transaction_counter += 1
-        time.sleep(5)
+    try:
+        while True:
+            event = {'transaction_id':f'trans_{transaction_counter}',
+                    'customer_id' : random.choice(customer_id),
+                    'timestamp' : time.time(),
+                    'product_id' : random.choice(product_id),
+                    'amount' : random.randint(100, 1000),
+                    'merchant_id' : random.choice(merchant_id)
+                    }
         
-except KeyboardInterrupt:
-    print('Stopped by User...')
+            with app.get_producer() as producer:
+                logging.info(f'got record for trans_{transaction_counter}')
 
+                producer.produce(topic='test-topic', value=json.dumps(event))
+
+                logging.info('Produced the record into kafka...sleeping...')
+
+            transaction_counter += 1
+            time.sleep(5)
+            
+    except KeyboardInterrupt:
+        print('Stopped by User...')
+
+if __name__ == '__main__':
+    logging.basicConfig(level="DEBUG")
+    main()
